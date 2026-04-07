d9299b284673667e649de67c6e9fea31b4bcf614
skomarov <skomarov@telematika.com>
2026-04-08 02:06:30 +0300

    hw03

--- hw03/src/main/java/ru/otus/hw/Application.java
+++ hw03/src/main/java/ru/otus/hw/Application.java
@@ -6,11 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-
-        //Создать контекст Spring Boot приложения
-//        ApplicationContext context = null;
-//        var testRunnerService = context.getBean(TestRunnerService.class);
-//        testRunnerService.run();
         SpringApplication.run(Application.class, args);
 
     }
